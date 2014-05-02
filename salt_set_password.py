ba9be1ae6ec2fdd7dc58ffa9fa4f5b42f74d21b0
Gohan <cppgohan@gmail.com>
2014-05-02 10:29:21 +0800

    支持了删除密码, 密码为空是, 提示删除密码

--- salt_set_password.py
+++ salt_set_password.py
@@ -2,12 +2,22 @@
 import crypt
 import random, string
 from subprocess import Popen, PIPE
-password = raw_input("Enter Password:")
-N = 8
-salt = ''.join(random.choice(
-    string.ascii_letters + string.digits) for _ in range(N))
-passhash = crypt.crypt(password, '$6$'+salt)
+
+def GetPasswordHash():
+    password = raw_input("Enter Password:")
+    if len(password) == 0:
+        clear = raw_input("Clear Password?[Y/n]")
+        clear = clear.lower().strip()
+        if len(clear) == 0 or clear[0] == 'y':
+            return "";
+    N = 8
+    salt = ''.join(random.choice(
+        string.ascii_letters + string.digits) for _ in range(N))
+    return crypt.crypt(password, '$6$'+salt)
+
 
 minion_filter = raw_input("Enter minion filter:");
 username = raw_input("Enter username:");
-print Popen(['sudo', 'salt', minion_filter, 'shadow.set_password', username, '"%s"' % passhash], stdout=PIPE).stdout.read()
+passhash = GetPasswordHash();
+
+print Popen(['sudo', 'salt', minion_filter, 'shadow.set_password', username, '%s' % passhash], stdout=PIPE).stdout.read()
